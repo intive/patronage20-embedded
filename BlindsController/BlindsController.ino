d3ea180850249a4fbe9802238d9caca7b0c96922
Kuba <siwyz@wp.pl>
2020-03-10 00:36:57 +0100

    [Ticket #991641] BlindsController update #1 - calibration module, manual control, serial port comunication
    Feature to be updated:
    -   Calibration module,
        setting range between fully open and fully closed, and move to position fully open.
    -   Manual control by user with two switch (blindsUP, blindsDOWN).
        Limitted by range setted in calibration module.
        Blinds movement can by stopped anytime by user manually (any switch).
    -   Using SerialPort to comunicate: calibration help, shoving currentValue.
        Can be replaced/improved by MQTT comunication etc. in next update.
    -   CurrentValue (value: 0-100 independent of range) - showed after any changes of position
        after stepper stoped (fully open, fully closed, stoped by user)
    -   module that lets move blinds into setted position (IN PROGRESS - not finished)

--- BlindsController/BlindsController.ino
+++ BlindsController/BlindsController.ino
@@ -1,45 +1,137 @@
 /* BlindsController by Kuba Bartoszewicz */
 
 /* defining pins for Stepper controller */
-#define Pin1 16 
-#define Pin2 5 
-#define Pin3 4 
-#define Pin4 0 
+#define IN1 16 
+#define IN2 5 
+#define IN3 4 
+#define IN4 0 
+
+#define stepperR() stepperRun((dir), (IN1), (IN2), (IN3), (IN4));
 
 /* defining pins for switch */
 #define SW1 14
+#define SW2 12
+
+
 
 /* initialize basic variables */
 int _step = 0; 
-boolean dir = false; /* false = clockwise, true=counter clockwise */
-int count = 0;
+boolean dir = false; /* false = blinds go DOWN, true = blinds go UP */
+int range = 0;
+int currentVal = 0;
+int i;
 
 void setup() 
 { 
-    pinMode(Pin1, OUTPUT);  
-    pinMode(Pin2, OUTPUT);  
-    pinMode(Pin3, OUTPUT);  
-    pinMode(Pin4, OUTPUT); 
+    Serial.begin(9600);
+    
+    pinMode(IN1, OUTPUT);  
+    pinMode(IN2, OUTPUT);  
+    pinMode(IN3, OUTPUT);  
+    pinMode(IN4, OUTPUT); 
     pinMode(SW1, INPUT_PULLUP); 
+    pinMode(SW2, INPUT_PULLUP); 
 
- 
+    /* Setting the range of blind */
+    range = blindsCalibration(SW2);
+    
 } 
 
 void loop() 
 { 
-    StepperRun();
-    
-    while (digitalRead(SW1) == LOW) {
-      dir = !dir;
-      delay(300);
+    /* manually move (by switch) blinds up until fully open or stopped by user */
+    if (digitalRead(SW1) == LOW) {
+        delay(300);
+        blindsUp(0);
+        Serial.println(currValPrc(currentVal));
     }
     
-    
+    /* manually move (by switch) blinds down until fully closed or stopped by user */
+    if (digitalRead(SW2) == LOW) {
+        delay(300);
+        blindsDown(range);
+        Serial.println(currValPrc(currentVal));
+    }
 
+    /* move blinds to specific value - outside signal (gatevay, serial port) */
+
+    /* IN PROGRESS */
+    
 }
 
-void StepperRun () {
+int currValPrc (float a) 
+{
+    float b = range;
+    float c = (a / b) * 100;
+    return c;
+}
+
+void blindsUp (int val) 
+{
+    dir = true;
     
+    for (int i = currentVal; i >= val; i--) {
+        stepperR();
+        currentVal = i;
+        if (digitalRead(SW1) == LOW || digitalRead(SW2) == LOW) {
+            delay(300);
+            break;
+        }
+    }
+}
+
+void blindsDown (int val) 
+{
+    dir = false;
+    
+    for (int i = currentVal; i <= val; i++) {
+        stepperR();
+        currentVal = i;
+        if (digitalRead(SW1) == LOW || digitalRead(SW2) == LOW) {
+            delay(300);
+            break;
+        }
+    }
+}
+
+int blindsCalibration (int controlSwitch) 
+{
+    int count = 0;
+    Serial.print("Press switch to start calibration:");
+    
+    while (digitalRead(controlSwitch) == HIGH) {
+        delay(10);
+    }
+    delay(300);
+    
+    Serial.println(" Starting calibration...");
+    Serial.println(" Blinds moving down - PRESS switch to stop.");
+    
+    while (dir == false) {
+        stepperR();
+        while (digitalRead(controlSwitch) == LOW) {
+            delay(300);
+            dir = !dir;
+        }
+    }
+    
+    Serial.println(" Blinds moving up - PRESS switch to stop.");
+    
+    while (dir == true) {
+        stepperR();
+        count++;
+        while (digitalRead(controlSwitch) == LOW) {
+            delay(300);
+            dir = !dir;
+        }
+    }
+    
+    Serial.println(count);
+    return count;
+}
+
+void stepperRun (boolean dir, int Pin1, int Pin2, int Pin3, int Pin4) 
+{    
     switch(_step){ 
         
         case 0: 
