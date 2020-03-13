baa08a313e195413a8f1cab190bd982b80516a2c
Szymon <szymon@debian.pl>
2020-03-13 14:37:10 +0100

    Added gateway

---
+++ gateway/gateway.cpp
@@ -0,0 +1,28 @@
+#include "dependencies/crow_all.h"
+#include "dependencies/json.hpp"
+
+int main()
+{
+    crow::SimpleApp gateway;
+    
+
+    CROW_ROUTE(gateway, "/hello")([](){
+        return "Hello world";
+    });
+
+    CROW_ROUTE(gateway, "/dashboard").methods(crow::HTTPMethod::Get)([](){
+        
+        crow::json::wvalue x;
+        nlohmann::json j;
+
+        std::ifstream i ("dashboard.json");
+        i>>j;
+        i.close();
+        
+        x = crow::json::load(j.dump());
+
+        return crow::response(x);
+    });
+
+    gateway.port(8080).multithreaded().run();
+}
