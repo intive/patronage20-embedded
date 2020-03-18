02c567c3d93a530017a6e991f39a71971f0e9f9b
Grzegorz Muth <grzesmuth@gmail.com>
2020-03-18 23:19:55 +0100

    [PATRON2020-25] Initial commit

---
+++ Motion_Sensor/MotionSensor.h
@@ -0,0 +1,31 @@
+#include <ArduinoJson.h>
+
+#define PIN 13
+
+/* returns true when motion is detected */
+/* and false when there is no motion */
+bool detect_motion()
+{
+    long state = digitalRead(PIN);
+  
+    if (state == HIGH)
+        return true;
+
+    else
+        return false;
+  }
+}
+
+/* Returns json string with sensor status */
+String motion_sensor()
+{
+    String output = "";
+    StaticJsonDocument<100> root;
+
+    root["id"] = 1;
+    root["type"] = "MotionSensor";
+    root["value"] = detect_motion();
+
+    serializeJson(root, output);
+    return output;
+}
---
+++ Motion_Sensor/MotionSensor.ino
@@ -0,0 +1,33 @@
+#include <ESP8266WiFi.h>
+#include "MotionSensor.h"
+#include "MQTT.h"
+
+#define PIN 13
+
+const char ssid[] = "ssid";
+const char passwd[] = "passwd";
+
+MQTT mqtt; 
+
+void setup()
+{
+    Serial.begin(115200);
+    pinMode(PIN, INPUT);
+    while (!Serial) {
+        ;
+    }
+    Serial.print("Trying to connect to: ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, passwd);
+    while (WiFi.status() != WL_CONNECTED) {
+        Serial.print(".");
+        delay(100);
+    }
+}
+
+void loop() 
+{
+    mqtt.loop();
+    mqtt.send(motion_sensor());
+    delay(100);
+}
