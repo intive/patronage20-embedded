734ca7f38a4422f4a1a19a07cb1b60ed019b0200
Grzegorz Muth <grzesmuth@gmail.com>
2020-03-19 12:10:10 +0100

    Update MotionSensor.h

--- Motion_Sensor/MotionSensor.h
+++ Motion_Sensor/MotionSensor.h
@@ -1,12 +1,10 @@
 #include <ArduinoJson.h>
 
-#define PIN 13
-
 /* returns true when motion is detected */
 /* and false when there is no motion */
-bool detect_motion()
+bool detect_motion(int pin)
 {
-    long state = digitalRead(PIN);
+    long state = digitalRead(pin);
   
     if (state == HIGH)
         return true;
@@ -16,14 +14,14 @@ bool detect_motion()
 }
 
 /* Returns json string with sensor status */
-String motion_sensor()
+String motion_sensor(int pin)
 {
     String output = "";
     StaticJsonDocument<100> root;
 
     root["id"] = 1;
     root["type"] = "MotionSensor";
-    root["value"] = detect_motion();
+    root["value"] = detect_motion(pin);
 
     serializeJson(root, output);
     return output;
