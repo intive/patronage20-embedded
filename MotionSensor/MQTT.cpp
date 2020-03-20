67b1b36b6e7141781f8599de813b22394bf6d51b
Grzegorz Muth <grzesmuth@gmail.com>
2020-03-20 16:31:59 +0100

    Added MQTT.cpp from Patryk

---
+++ MotionSensor/MQTT.cpp
@@ -0,0 +1,64 @@
+#include "MQTT.h"
+
+void MQTT::reconnect() {
+    /* Loop until we're reconnected */
+    while (!client.connected()) {
+        #if DEBUG==1
+        Serial.print("Attempting MQTT connection...");
+        #endif
+      /* Attempt to connect */
+        if (client.connect(serialnumber)) {
+            #if DEBUG==1
+            Serial.println("connected");
+            #endif
+            client.subscribe(topic);
+        } else {
+            #if DEBUG==1
+            Serial.print("failed, rc=");
+            Serial.print(client.state());
+            Serial.println(" try again in 5 seconds");
+            #endif
+          /* Wait 5 seconds before retrying */
+            delay(5000);
+      }
+    }
+  }
+
+/*  Gets whole traffic from MQTT and interprets it */
+void MQTT::callback(char* topic, byte* payload, unsigned int length) {
+  /* DEBUG */
+    #if DEBUG==1
+    Serial.print("\nMessage arrived [");
+    Serial.print(topic);
+    Serial.print("] ");
+    #endif
+    /*End of debug*/
+    
+    String payload_string = "";
+    for (int i=0;i<length;i++) {
+        payload_string+=(char)payload[i];
+    }
+    #if DEBUG==1
+    Serial.print(payload_string);
+    #endif
+    if(returnFunct!=nullptr)
+        returnFunct(payload_string);
+}
+
+/*  This should be called in loop function in main device file  */
+void MQTT::loop(){
+    if(WiFi.isConnected())
+        if (!client.connected())
+            reconnect();
+    
+    client.loop();
+}
+
+/*  In constructot a library configures mqtt and setup callback function  */
+MQTT::MQTT(){
+    client.setClient(wifiClient);
+    client.setServer(broker, port);
+
+    std::function<void(char*, uint8_t*, unsigned int)> WhereCatsDoMEWMEW = std::bind(&MQTT::callback, this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3);
+    client.setCallback(WhereCatsDoMEWMEW);
+}
