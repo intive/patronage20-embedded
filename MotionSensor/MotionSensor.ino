5bfc7bf8aab2dd51ba019a92472f01a3e99995e2
Grzegorz Muth <grzesmuth@gmail.com>
2020-03-26 22:30:08 +0100

    Remove the MQTT feature for now.

--- MotionSensor/MotionSensor.ino
+++ MotionSensor/MotionSensor.ino
@@ -1,33 +1,16 @@
 #include <ESP8266WiFi.h>
 #include "MotionSensor.h"
-#include "MQTT.h"
 
 #define PIN 13
-
-const char ssid[] = "ssid";
-const char passwd[] = "passwd";
-
-MQTT mqtt; 
 
 void setup()
 {
     Serial.begin(115200);
     pinMode(PIN, INPUT);
-    while (!Serial) {
-        delay(100);
-    }
-    Serial.print("Trying to connect to: ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, passwd);
-    while (WiFi.status() != WL_CONNECTED) {
-        Serial.print(".");
-        delay(100);
-    }
 }
 
-void loop() 
+void loop()
 {
-    mqtt.loop();
-    mqtt.send(motion_sensor(PIN));
+    Serial.println(motion_sensor(PIN));
     delay(100);
 }
