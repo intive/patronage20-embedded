5d15032627495c9d18d633e17875a80ab34f9922
Artur <R_E_D_O_X@wp.pl>
2020-04-17 15:11:38 +0200

    PATRON2020-194 Notyfikowanie temperatury przez moduł termometru za pośrednictwem MQTT

--- substations/07_TemperatureSensor/07_TemperatureSensor.ino
+++ substations/07_TemperatureSensor/07_TemperatureSensor.ino
@@ -1,27 +1,50 @@
-/* Demonstration how to use read_analog_temperature and read_digital_temperature function */
+/* Temperature sensor */
 
 #include "getTemperature.h"
 #include "esp_pins.h"
+#include "init_config.h"
+#include "MQTT.h"
+#include "Network.h"
 
-int digital_pin = D2; 
-int analog_pin = A0; 
+/* Network connecion library - ssid and password got from init_config.h */
+Network network(ssid, passwd);
 
-void setup() 
+/* Communication library */
+MQTT mqtt;
+
+int digital_pin = D2;
+int analog_pin = A0;
+
+/* Function to reporting temperature to the broker */
+void raport_temperature(String json_output)
 {
-    Serial.begin(9600); 
-    while (!Serial) {    
+    /* Send JSON object over the MQTT */
+    mqtt.send(json_output);
+}
+
+void setup()
+{
+    Serial.begin(9600);
+    while (!Serial) {
     }
 
     pinMode(digital_pin, INPUT);
     pinMode(D8, INPUT);
+
+    /* Initializing Wifi network */
+    network.init();
 }
 
-void loop() 
+void loop()
 {
-    Serial.print("Digital temperature: ");
-    Serial.println(read_digital_temperature(digital_pin));
-    Serial.print("Analog temperature: ");
-    Serial.println(read_analog_temperature(analog_pin));
-    if(digitalRead(D8)==HIGH )Serial.println("high");
-    delay(2000);
+    /* Keep network alive */
+    network.loop();
+    /* Keep MQTT alive */
+    mqtt.loop();
+    /* Report temperature from digital sensor to the broker */
+    raport_temperature(get_temperature(true, digital_pin));
+    /* Report temperature from analog sensor to the broker */
+    raport_temperature(get_temperature(false, analog_pin));
+
+    delay(1000);
 }
