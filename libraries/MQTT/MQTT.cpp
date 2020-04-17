ebd9f5d68ef53bc9e830bfe1eeb7d53b763a1686
Artur <R_E_D_O_X@wp.pl>
2020-04-17 16:11:54 +0200

    PATRON2020-203 Configure TLS for MQTT

--- libraries/MQTT/MQTT.cpp
+++ libraries/MQTT/MQTT.cpp
@@ -33,7 +33,7 @@ void MQTT::callback(char* topic, byte* payload, unsigned int length) {
     Serial.print("] ");
     #endif
     /*End of debug*/
-    
+
     String payload_string = "";
     for (int i=0;i<length;i++) {
         payload_string+=(char)payload[i];
@@ -48,9 +48,12 @@ void MQTT::callback(char* topic, byte* payload, unsigned int length) {
 /*  This should be called in loop function in main device file  */
 void MQTT::loop(){
     if(WiFi.isConnected())
-        if (!client.connected())
+        if (!client.connected()) {
+            /* Load a certificate before connection, the ESP clock is set */
+            load_certificate();
             reconnect();
-    
+        }
+
     client.loop();
 }
 
@@ -61,4 +64,71 @@ MQTT::MQTT(){
 
     std::function<void(char*, uint8_t*, unsigned int)> WhereCatsDoMEWMEW = std::bind(&MQTT::callback, this,std::placeholders::_1,std::placeholders::_2,std::placeholders::_3);
     client.setCallback(WhereCatsDoMEWMEW);
+}
+
+void MQTT::load_certificate() {
+    /* Set the ESP clock time, needed for certificate authorization */
+    set_clock();
+
+    /* Try to mount SPIFFS file system to load certificate */
+    if (!SPIFFS.begin()) {
+        #if DEBUG==1
+        Serial.println("Failed to mount file system");
+        #endif
+        return;
+    }
+
+    /* Try to load CA file from SPIFFS */
+    File ca = SPIFFS.open("/CA.crt", "r");
+    if (!ca) {
+        #if DEBUG==1
+        Serial.println("Failed to open certificate");
+        #endif
+    }
+    else {
+        #if DEBUG==1
+        Serial.println("Success to open certificate");
+        #endif
+        delay(1000);
+        /* Set server certificate file */
+        if(wifiClient.loadCACert(ca)) {
+            #if DEBUG==1
+            Serial.println("Certificate loaded");
+            #endif
+        }
+        else {
+            #if DEBUG==1
+            Serial.println("Load certificate failed");
+            #endif
+        }
+    }
+}
+void MQTT::set_clock() {
+    /* Set timezone, day light offset and time servers */
+    configTime(2 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+
+    #if DEBUG==1
+    Serial.print("Waiting for NTP time sync: ");
+    #endif
+
+    time_t now = time(nullptr);
+    /* Until time synchronization is complete, the time(nullptr) function returns time around unix zero time*/
+    while (now < 8 * 3600 * 2) {
+        delay(500);
+
+        #if DEBUG==1
+        Serial.print(".");
+        #endif
+
+        now = time(nullptr);
+    }
+    #if DEBUG==1
+    Serial.println("");
+
+    /* Print time to serial */
+    struct tm timeinfo;
+    gmtime_r(&now, &timeinfo);
+    Serial.print("Current time: ");
+    Serial.print(asctime(&timeinfo));
+    #endif
 }
