31b709951ff5b22a52a3c50c4659aa2737e4b94c
Artur <R_E_D_O_X@wp.pl>
2020-04-21 17:32:19 +0200

    Removal of unnecessary comments and the report_temperature function. Putting ID in init_config.h

--- libraries/TemperatureSensor/getTemperature.h
+++ libraries/TemperatureSensor/getTemperature.h
@@ -1,31 +1,30 @@
 #ifndef GETTEMPERATURE_H
 #define GETTEMPERATURE_H
 
-/* Unique values to each Temperature sensor */
-#define ID 1
-#define TYPE "TEMPERATURE_SENSOR"
-
 /* Function for reading digital or analog temperature */
 
 #include <ArduinoJson.h>
 #include <math.h>
 #include "ReadDigitalTemperature.h"
 #include "ReadAnalogTemperature.h"
+#include "init_config.h"
 
 String get_temperature(bool digital, int in_pin)
 {
     DynamicJsonDocument root(200);
     String json_output = "";
     /* Preparing JSON to send to the broker */
-    root["id"] = ID;
-    root["type"] = TYPE;
-       
+
+    /* Unique values to each Temperature sensor ID and TYPE included in init_config.h */
+    root["id"] = id_temp_sensor_1;
+    root["type"] = type_temp_sensor;
+
     if(digital) {
         root["value"] = read_digital_temperature(in_pin);
     }else {
         root["value"] = read_analog_temperature(in_pin);
     }
-    serializeJson(root, json_output); 
+    serializeJson(root, json_output);
     return json_output;
 }
 #endif /* GETTEMPERATURE_H */
--- substations/07_TemperatureSensor/07_TemperatureSensor.ino
+++ substations/07_TemperatureSensor/07_TemperatureSensor.ino
@@ -6,44 +6,34 @@
 #include "MQTT.h"
 #include "Network.h"
 
-/* Network connecion library - ssid and password got from init_config.h */
+/* ssid and password got from init_config.h */
 Network network(ssid, passwd);
 
-/* Communication library */
 MQTT mqtt;
 
 int digital_pin = D2;
 int analog_pin = A0;
-
-/* Function to reporting temperature to the broker */
-void raport_temperature(String json_output)
-{
-    /* Send JSON object over the MQTT */
-    mqtt.send(json_output);
-}
 
 void setup()
 {
     Serial.begin(9600);
     while (!Serial) {
     }
+
     pinMode(digital_pin, INPUT);
     pinMode(D8, INPUT);
 
-    /* Initializing Wifi network */
     network.init();
 }
 
 void loop()
 {
-    /* Keep network alive */
     network.loop();
-    /* Keep MQTT alive */
     mqtt.loop();
     /* Report temperature from digital sensor to the broker */
-    raport_temperature(get_temperature(true, digital_pin));
+    mqtt.send(get_temperature(true, digital_pin));
     /* Report temperature from analog sensor to the broker */
-    raport_temperature(get_temperature(false, analog_pin));
+    mqtt.send(get_temperature(false, analog_pin));
 
     delay(1000);
 }
