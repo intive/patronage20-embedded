34ad103258fd7f36b0fa8ac04beccee1b611020a
Artur <R_E_D_O_X@wp.pl>
2020-04-22 11:35:08 +0200

    Placing the report_hsv_color function into header file, placing ID and TYPE in init_config.h, removing unnecessary comments

--- substations/02_LedControlerRGB/02_LedControlerRGB.ino
+++ substations/02_LedControlerRGB/02_LedControlerRGB.ino
@@ -1,72 +1,44 @@
 /* LED controller */
-
 #include <ArduinoJson.h>
 #include "HSVtoRGB.h"
 #include "esp_pins.h"
 #include "init_config.h"
 #include "MQTT.h"
 #include "Network.h"
-
-/* Unique values to each LED controller */
-#define ID 1
-#define TYPE "LED_CONTROLLER"
+#include "report_hsv_color.h"
 
 /* Varibles to store a color */
 int hue = 0, saturation = 0, value = 0;
 
-/* Communication library */
 MQTT mqtt;
-/* Network connecion library - ssid and password got from init_config.h */
+/* ssid and password got from init_config.h */
 Network network(ssid, passwd);
-
-void raport_hsv_color()
-{
-    DynamicJsonDocument root(200);
-    String json_output = "";
-    /* Preparing JSON object to send to the broker */
-    root["id"] = ID;
-    root["type"] = TYPE;
-    root["hue"] = hue;
-    root["saturation"] = saturation;
-    root["value"] = value;
-    serializeJson(root, json_output);
-    /* Send JSON object over the MQTT */
-    mqtt.send(json_output);
-}
 
 /* Callback function for MQTT library */
 void get_hsv_color(String json_input)
 {
     DynamicJsonDocument json_doc(2048);
     DeserializationError error = deserializeJson(json_doc, json_input);
-    /* If parsing json_doc throws error, return get_hsv_color function */
     if (error)
         return;
-    /* Checking if the message concerns our controller */
-    if(json_doc["id"].as<int>() == ID)
-        if(json_doc["type"].as<String>().equals(TYPE))
+    /* id_led_controller_1, type_led_controller from init_config.h */
+    if(json_doc["id"].as<int>() == id_led_controller_1)
+        if(json_doc["type"].as<String>().equals(type_led_controller))
         {
-            /* Store recivied color */
             hue = json_doc["hue"].as<int>();
             saturation = json_doc["saturation"].as<int>();
             value = json_doc["value"].as<int>();
-            /* Setting LED the color received by MQTT */
             set_led_rgb_value(D6, D5, D7);
         }
 }
 
-/* Function to control led based on the received from MQTT hue, saturation and value */
 void set_led_rgb_value(int red_pin, int green_pin, int blue_pin)
 {
     int rgb[3];
-
-    /* Convert HSV to RGB color */
     hsv_to_rgb(hue, saturation, value, rgb);
-
-    analogWrite(red_pin,rgb[0]);        /* set PWM value for red */
-    analogWrite(green_pin, rgb[1]);     /* set PWM value for blue */
-    analogWrite(blue_pin, rgb[2]);      /* set PWM value for green */
-
+    analogWrite(red_pin,rgb[0]);
+    analogWrite(green_pin, rgb[1]);
+    analogWrite(blue_pin, rgb[2]);
 }
 
 void setup()
@@ -75,19 +47,14 @@ void setup()
     pinMode(D5, OUTPUT);
     pinMode(D6, OUTPUT);
     pinMode(D7, OUTPUT);
-    /* Initializing Wifi network */
     network.init();
-    /* Set callback function geting HSV color for MQTT */
     mqtt.set_return_function(get_hsv_color);
 }
 
 void loop()
 {
-    /* Keep network alive */
     network.loop();
-    /* Keep MQTT alive */
     mqtt.loop();
-    /* Report HSV color to the broker */
-    raport_hsv_color();
+    mqtt.send(report_hsv_color(hue, saturation, value, id_led_controller_1, type_led_controller));
     delay(1000);
 }
