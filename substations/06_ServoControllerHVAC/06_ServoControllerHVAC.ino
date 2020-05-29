e4a3899b7cdfacf6bc53d421e75a300fbd8eec81
Jakub Bartoszewicz <j_bartoszewicz@wp.pl>
2020-05-29 12:06:23 +0200

    fix to avoid switch

--- substations/06_ServoControllerHVAC/06_ServoControllerHVAC.ino
+++ substations/06_ServoControllerHVAC/06_ServoControllerHVAC.ino
@@ -4,6 +4,8 @@
 #include "Network.h"
 #include <ArduinoJson.h>
 
+#define NO_OF_SERVOS 4
+
 /* defining PIN's for Servo */
 #define SERVO_1H_PIN 16 /* D0 */
 #define SERVO_1C_PIN 5  /* D1 */
@@ -11,40 +13,35 @@
 #define SERVO_2C_PIN 0  /* D3 */
 
 /* defining ID's for Servos */
-const int id_1H = Servo_1H_ID; /* 101 */
-const int id_1C = Servo_1C_ID; /* 102 */
-const int id_2H = Servo_2H_ID; /* 103 */
-const int id_2C = Servo_2C_ID; /* 104 */
+/* order in array: Room 1 heating, Room 1 cooling, Room 2 heating, Room 2 cooling */
+const int servo_ID[NO_OF_SERVOS] = {Servo_1H_ID, Servo_1C_ID, Servo_2H_ID, Servo_2C_ID};
 
-/* variables storing angles for servo's */
-int ang_1H;
-int ang_1C;
-int ang_2H;
-int ang_2C;
-
-String type = "Servo";
+/* variable storing angles for servo's */
+int servo_ANGLE[NO_OF_SERVOS];
 
 MQTT mqtt;
 Network network(ssid, passwd);
 
+/* servo objects */
 Servo servo_1H;
 Servo servo_1C;
 Servo servo_2H;
 Servo servo_2C;
 
+String type = "Servo";
 int notifCaller = 0;
 
 int getPin_fromServoID(int id) 
 {
     switch(id) {
-        case id_1H:  return SERVO_1H_PIN;
-        case id_1C:  return SERVO_1C_PIN;
-        case id_2H:  return SERVO_2H_PIN;
-        case id_2C:  return SERVO_2C_PIN;
+        case Servo_1H_ID:  return SERVO_1H_PIN;
+        case Servo_1C_ID:  return SERVO_1C_PIN;
+        case Servo_2H_ID:  return SERVO_2H_PIN;
+        case Servo_2C_ID:  return SERVO_2C_PIN;
     }
 }
 
-String output_JSON(int a, int id, String t)
+String output_JSON(int a, int id, String t = type)
 {
     DynamicJsonDocument root(200);
     String json_output = "";
@@ -57,42 +54,41 @@ String output_JSON(int a, int id, String t)
 
 void incoming_JSON(String json_input)
 {
-    int id;
+    int i;
+    
     DynamicJsonDocument json_doc(2048);
     DeserializationError error = deserializeJson(json_doc, json_input);
     
     if (error) {
         return;
     }
-    
-    id = json_doc["id"].as<int>();
-
-    
+       
     if(json_doc["type"].as<String>().equals(type) 
        && json_doc["angle"] >= 0 
        && json_doc["angle"] <=180) {
-        
-        switch(id) {
-            case id_1H: ang_1H = json_doc["angle"]; servo_1H.write(ang_1H); break;
-            case id_1C: ang_1C = json_doc["angle"]; servo_1C.write(ang_1C);break;
-            case id_2H: ang_2H = json_doc["angle"]; servo_2H.write(ang_2H);break;
-            case id_2C: ang_2C = json_doc["angle"]; servo_2C.write(ang_2C);break;
+
+        for (i = 0; i <= NO_OF_SERVOS; i++) {
+            if (servo_ID[i] == json_doc["id"].as<int>()) {
+                servo_ANGLE[i] = json_doc["angle"];
+            }
         }
     }
 }
 
 void setup()
 {
-    servo_1H.attach(getPin_fromServoID(id_1H));
-    servo_1C.attach(getPin_fromServoID(id_1C));
-    servo_2H.attach(getPin_fromServoID(id_2H));
-    servo_2C.attach(getPin_fromServoID(id_2C));
+    int i;
     
+    servo_1H.attach(getPin_fromServoID(servo_ID[0]));
+    servo_1C.attach(getPin_fromServoID(servo_ID[1]));
+    servo_2H.attach(getPin_fromServoID(servo_ID[2]));
+    servo_2C.attach(getPin_fromServoID(servo_ID[3]));
+     
     servo_1H.write(10);
     servo_1C.write(10);
     servo_2H.write(10);
-    servo_2C.write(10);
-    
+    servo_2H.write(10);
+ 
     Serial.begin(9600);
     network.init();
     mqtt.set_return_function(incoming_JSON);
@@ -100,13 +96,19 @@ void setup()
 
 void loop()
 {
+    int i;
+    
     mqtt.loop();
 
+    servo_1H.write(servo_ANGLE[0]);
+    servo_1C.write(servo_ANGLE[1]);
+    servo_2H.write(servo_ANGLE[2]);
+    servo_2C.write(servo_ANGLE[3]);
+    
     if (++notifCaller == 1000) {
-        mqtt.send(output_JSON(ang_1H, id_1H, type));
-        mqtt.send(output_JSON(ang_1C, id_1C, type));
-        mqtt.send(output_JSON(ang_2H, id_2H, type));
-        mqtt.send(output_JSON(ang_2C, id_2C, type));
+        for (i = 0; i < NO_OF_SERVOS; i++) {
+            mqtt.send(output_JSON(servo_ANGLE[i], servo_ID[i]));
+        }
         notifCaller = 0;
     }
 
